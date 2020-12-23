ee8e4071aa383f8a5ebe92b112a18662b61493f8
MMCH <john.doe@example.com>
2020-12-23 12:51:20 +0100

    huit

--- getBalance.c
+++ getBalance.c
@@ -4,35 +4,34 @@
 #include <stdlib.h>
 
 typedef struct {
-    unsigned char *buffer;
-    size_t len;
-    size_t buflen;
+	unsigned char *buffer;
+	size_t len;
+	size_t buflen;
 } get_request;
 
 typedef struct {
 	size_t start;
-    size_t len;
+	size_t len;
 } start_len;
 
 #define CHUNK_SIZE 2048
 
-size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
+size_t	write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
 {
-    size_t realsize = size * nmemb;
-    get_request *req = (get_request *) userdata;
+	size_t realsize = size * nmemb;
+	get_request *req = (get_request *) userdata;
 
-    // printf("receive chunk of %zu bytes\n", realsize);
+	// printf("receive chunk of %zu bytes\n", realsize);
+	while (req->buflen < req->len + realsize + 1)
+	{
+		req->buffer = realloc(req->buffer, req->buflen + CHUNK_SIZE);
+		req->buflen += CHUNK_SIZE;
+	}
+	memcpy(&req->buffer[req->len], ptr, realsize);
+	req->len += realsize;
+	req->buffer[req->len] = 0;
 
-    while (req->buflen < req->len + realsize + 1)
-    {
-        req->buffer = realloc(req->buffer, req->buflen + CHUNK_SIZE);
-        req->buflen += CHUNK_SIZE;
-    }
-    memcpy(&req->buffer[req->len], ptr, realsize);
-    req->len += realsize;
-    req->buffer[req->len] = 0;
-
-    return realsize;
+	return realsize;
 }
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
@@ -58,7 +57,7 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 	return (NULL);
 }
 
-start_len ft_truncate(char *buffer, int delimiter, int x) // this function calculate where to start and the len of the return
+start_len ft_truncate(char *buffer, int delimiter, int x) // this function calculate where to start and where to end
 {
 	int		j;
 	int		k;
@@ -80,50 +79,47 @@ start_len ft_truncate(char *buffer, int delimiter, int x) // this function calcu
 	return (data);
 }
 
-int main(int argc, char *argv[])
+void getBalance(char *argv)
 {
-  char  int_getBalance[500];
-  char  *result;
-  start_len data = {.start = 0, .len = 0};
+	char		int_getBalance[500];
+	char		*result;
+	start_len	data = {.start = 0, .len = 0};
+	get_request req = {.buffer = NULL, .len = 0, .buflen = 0};
+	CURLcode ret;
+	CURL *hnd;
+	struct curl_slist *slist1;
+	slist1 = NULL;
+	slist1 = curl_slist_append(slist1, "content-type: application/json;");
+	hnd = curl_easy_init();
 
-  sprintf(int_getBalance, "{\"jsonrpc\":\"2.0\",\"method\":\"int_getBalance\",\"params\":[\"%s\", \"latest\"],\"id\":1}", argv[1]);
+	sprintf(int_getBalance, "{\"jsonrpc\":\"2.0\",\"method\":\"int_getBalance\",\"params\":[\"%s\", \"latest\"],\"id\":1}", argv);
 
-  get_request req = {.buffer = NULL, .len = 0, .buflen = 0};
+	curl_easy_setopt(hnd, CURLOPT_BUFFERSIZE, 102400L);
+	curl_easy_setopt(hnd, CURLOPT_URL, "http://localhost:8555/testnet");
+	curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, int_getBalance);
+	curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, slist1);
+	curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
+	req.buffer = malloc(CHUNK_SIZE);
+	req.buflen = CHUNK_SIZE;
+	curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, write_callback);
+	curl_easy_setopt(hnd, CURLOPT_WRITEDATA, (void *)&req);
 
-  CURLcode ret;
-  CURL *hnd;
-  struct curl_slist *slist1;
+	ret = curl_easy_perform(hnd);
+	// printf("Result = %u\n",ret);
+	// printf("Total received bytes: %zu\n", req.len);
+	data = ft_truncate(req.buffer, '"', 9);
+	result = ft_strsub(req.buffer, data.start, data.len);
+	printf("Received data: %s\n", result);
+	free(req.buffer);
 
-  slist1 = NULL;
-  slist1 = curl_slist_append(slist1, "content-type: application/json;");
+	curl_easy_cleanup(hnd);
+	hnd = NULL;
+	curl_slist_free_all(slist1);
+	slist1 = NULL;
+}
 
-  hnd = curl_easy_init();
-  curl_easy_setopt(hnd, CURLOPT_BUFFERSIZE, 102400L);
-  curl_easy_setopt(hnd, CURLOPT_URL, "http://localhost:8555/testnet");
-  // curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
-  curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, int_getBalance);
-  curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, slist1);
-  curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
-
-  req.buffer = malloc(CHUNK_SIZE);
-  req.buflen = CHUNK_SIZE;
-
-  curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, write_callback);
-  curl_easy_setopt(hnd, CURLOPT_WRITEDATA, (void *)&req);
-
-  ret = curl_easy_perform(hnd);
-  // printf("Result = %u\n",ret);
-
-  // printf("Total received bytes: %zu\n", req.len);
-  data = ft_truncate(req.buffer, '"', 9);
-  result = ft_strsub(req.buffer, data.start, data.len);
-  printf("Received data: %s\n", result);
-  free(req.buffer);
-
-  curl_easy_cleanup(hnd);
-  hnd = NULL;
-  curl_slist_free_all(slist1);
-  slist1 = NULL;
-
-  return (int)ret;
+int	main(int argc, char *argv[])
+{
+	getBalance(argv[1]);
+	return (0);
 }
